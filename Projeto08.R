04e1655b79e074a6f7a7b6d45472f41601208aa7
joaolAtaide <joaol.ataide@gmail.com>
2022-11-29 10:32:27 -0300

    Atualizado 29/11

--- Projeto08.R
+++ Projeto08.R
@@ -3,6 +3,7 @@
 
 install.packages("tidyverse")
 library(tidyverse)
+library(ggplot2)
 
 ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy))
 
@@ -15,3 +16,28 @@ ggplot(data=mpg) + geom_point(mapping = aes(x = class,y = drv))
 ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy, color = class)) #Fica com cor
 
 ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy, shape = class)) #Só tem 6 shapes.
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy, alpha = class)) #Muda transparência, Difícil de ver
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy, size = class)) #Muda tamanho, Também ruim de ver
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy), color = "blue")
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy, linetype=drv))
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy))
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy)) + geom_smooth(mapping=aes(x=displ, y=hwy))
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy)) + geom_smooth(mapping=aes(x=displ, y=hwy), se=FALSE)
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy, line=drv), se=FALSE) + geom_point(mapping = aes(x = displ,y = hwy))
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy, line=drv, color=drv), se=FALSE) + geom_point(mapping = aes(x = displ,y = hwy, color=drv))
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy), se=FALSE) + geom_point(mapping = aes(x = displ,y = hwy, color=drv))
+
+ggplot(data=mpg) + geom_smooth(mapping=aes(x=displ, y=hwy, linetype=drv), se=FALSE) + geom_point(mapping = aes(x = displ,y = hwy, color=drv))
+
+ggplot(data=mpg) + geom_point(mapping = aes(x = displ,y = hwy, color=drv)) + geom_point(mapping = aes(x = displ,y = hwy, color=drv), colour='white', pch=21, size=3)
+
